b856eb24b874df67a791186a3c74c13815dd859f
mfahadakbar <fahadakbar@gmail.com>
2019-11-25 12:26:04 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
-#requirements = ["ipython>=6"] 
+requirements = ["ipython>=6"] 
 # this is where you mention dependencies for your package
 # only add libraries that are not by default installed by python , so if your package only
 # used say pandas or numpy , you dont need to put them here as they are already installed 
 
 setup(
     name="preprocess1",
-    version="0.0.4",
+    version="0.0.9",
     author="Fahad Akbar",
     author_email="fahadakbar@gmail.com",
     description="A package to eliminate multicollinearity",
@@ -15,7 +15,7 @@ setup(
     #long_description_content_type="text/markdown",
     url="https://github.com/mfahadakbar",
     packages=find_packages(),
-    #install_requires=requirements,
+    install_requires=requirements,
     #classifiers=[
     #    "Programming Language :: Python :: 3.7",
     #    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
