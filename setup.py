0cf847a40f70ab9279ef5e3171dfb78a3e762278
mfahadakbar <fahadakbar@gmail.com>
2020-04-29 00:50:59 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
-requirements = ["scikit-learn","datetime","datefinder","pyod"] # add plotly later on
+requirements = ["scikit-learn","datetime","datefinder","pyod","lightgbm"] # add plotly later on
 
 
 setup(
     name="preprocess1",
-    version="0.1.34",
+    version="0.1.35",
     author="Fahad Akbar",
     author_email="fahadakbar@gmail.com",
     description="A package to provide preprocessing steps for Machine Learning in an super easy way !",
